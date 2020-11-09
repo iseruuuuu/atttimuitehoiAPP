9d4b9d69e5dbc109b521f7f3c5c4457c77dbb9e7
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-11-09 19:37:55 +0900

    Initial Commit

---
+++ atttimuitehoiAPP/atttimuitehoiAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  atttimuitehoiAPPApp.swift
+//  atttimuitehoiAPP
+//
+//  Created by user on 2020/11/09.
+//
+
+import SwiftUI
+
+@main
+struct atttimuitehoiAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
